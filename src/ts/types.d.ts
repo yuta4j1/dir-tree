77a2b5a5db52c9e39ac5aa988ef75f4c19697029
Ozaki Yoshiaki <kascado.ys10@gmail.com>
2019-05-04 22:57:40 +0900

    add: types 'DirNodeSrc', 'ReadDirFilter'

--- src/ts/types.d.ts
+++ src/ts/types.d.ts
@@ -1,21 +1,36 @@
 // ファイルorディレクトリ
-export interface DirNode {
+export type DirNode = {
   nodeName: string;
   absolutePath: string;
   isDirectory: boolean;
-}
+};
 
 // 階層あたりのファイルorディレクトリ群
-export interface DirLayerNodes {
+export type DirLayerNodes = {
   parentDirName: string;
   nodes: DirNode[];
   layerNum: number;
-}
+};
 
 // 表示用ファイル情報
-export interface ALine {
+export type ALine = {
   index: number;
   path: string;
   displayStr: string;
   isDirectory: boolean;
-}
+};
+
+// オプション
+export type Option = {
+  directoryOnly?: boolean;
+  pattern?: string | undefined;
+};
+
+// ディレクトリ情報読み出し時にfilterする用の型
+type DirNodeSrc = {
+  p: string;
+  n: string;
+};
+
+// ディレクトリ読み出し時のfilter関数
+export type ReadDirsFilter = (src: DirNodeSrc) => boolean;
