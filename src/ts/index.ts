b9e16fcca0b0fdf75c9a80a218bc90639881ccf0
Ozaki Yoshiaki <kascado.ys10@gmail.com>
2019-05-04 22:58:39 +0900

    feat: option '--directory', '--exp'

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,18 +1,29 @@
 import { createDirTree } from './dirTree';
 import { consoleDisplay } from './helper';
+import { Option } from './types';
 
 // エントリポイント
 const entry = (): void => {
   var program = require('commander');
   program
-    .option('-P, --path [path]', 'Add path')
-    .option('-L, --log [logPath]', 'Add logfile path')
+    .option('-P, --path [path]', 'target root path')
+    .option('-L, --log [logPath]', 'logfile path to output directory tree')
+    .option('-d --directory', 'directory only')
+    .option('-E --exp [exp]', 'pattern match')
     .parse(process.argv);
+  // ルートパスの必須チェック
   if (!program.path) {
     throw new Error('Illegal arguments.');
   }
   const rootPath = program.path;
-  const arr: string[] = createDirTree(rootPath);
+  // オプション
+  const option: Option = {
+    directoryOnly: program.directory || false,
+    pattern: program.exp || undefined
+  };
+  // 出力できるよう、ディレクトリ情報を読み出し、ツリー形式へパース
+  const arr: string[] = createDirTree(rootPath, option);
+  // ディレクトリツリー出力
   consoleDisplay(arr);
 };
 
